517f2a9e0a9db9d9f1651ff55a0c335e23194b36
botcerts <botcerts@learningmachine.com>
2024-10-21 16:49:18 +0200

    chore(CI): debug 'no such file' error

--- release_npm_package.sh
+++ release_npm_package.sh
@@ -4,7 +4,7 @@ cd npm
 npm ci
 npm run copy:schemas
 npm publish
+cd ..
 pwd
 ls
-'if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then sh ../scripts/automated-update-blockcerts.org.sh; fi' # if no changes (aka npm release), no commit then no PR
-cd ..
+'if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then sh ./scripts/automated-update-blockcerts.org.sh; fi' # if no changes (aka npm release), no commit then no PR
