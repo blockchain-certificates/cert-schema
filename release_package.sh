771226f8152f91fa8322099a4c4c7200902e29e6
botcerts <botcerts@learningmachine.com>
2024-10-21 17:41:15 +0200

    chore(CI): fix typo

--- release_package.sh
+++ release_package.sh
@@ -5,4 +5,4 @@ python3 setup.py sdist bdist_wheel
 keyring --disable
 echo $TWINE_USERNAME
 echo $TWINE_PASSWORD
-twine upload dist/* -u $TWINE_USERNAME} -p $TWINE_PASSWORD --verbose
+twine upload dist/* -u $TWINE_USERNAME -p $TWINE_PASSWORD --verbose
