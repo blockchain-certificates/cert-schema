690b3b8e2c2c3340c04608239e078099fb84cbbd
botcerts <botcerts@learningmachine.com>
2025-07-21 16:55:10 +0200

    fix(Node22): change assert to with

--- npm/index.mjs
+++ npm/index.mjs
@@ -1,4 +1,4 @@
-import CONTEXT_URLS from './schemas/context_urls.json' assert { type: 'json'};
+import CONTEXT_URLS from './schemas/context_urls.json' with { type: 'json'};
 import preloadedContexts from "./preloadedContexts.mjs";
 
 export {
